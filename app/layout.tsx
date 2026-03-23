e65d59a37bb262a214cb06a3cb054e6b84a3aec6
Eugene Kyale <eugene.kyale@gmail.com>
2026-03-23 15:38:44 +0300

    Add ThemeFavicon component and SVG icon; update layout to include favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SiteFooter } from '@/components/site-footer';
+import { ThemeFavicon } from '@/components/theme-favicon';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Lexend } from 'next/font/google';
@@ -11,8 +12,9 @@ const lexend = Lexend({
 export default function Layout({ children }: LayoutProps<'/'>) {
 	return (
 		<html lang="en" className={lexend.className} suppressHydrationWarning>
-			<body className="flex min-h-screen flex-col">
+			<body className="flex min-h-screen flex-col" suppressHydrationWarning>
 				<RootProvider>
+					<ThemeFavicon />
 					<div className="flex min-h-screen flex-1 flex-col">
 						<div className="flex min-h-0 min-w-0 flex-1 flex-col">
 							{children}
